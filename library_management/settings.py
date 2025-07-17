fd04e4ae949c8b478a3326aaf24adb811434826b
Nazmul Hossain <snazmulhossains24@gmail.com>
2025-07-17 20:07:38 +0600

    Configure DRF and Nested_router+ Root page...

--- library_management/settings.py
+++ library_management/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     #
+    "rest_framework",
     "debug_toolbar",
     #
     "api",
@@ -137,3 +138,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "COERCE_DECIMAL_TO_STRING": False,
+}
