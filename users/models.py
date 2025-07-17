299f025a9e320cc6963b9bca50aa06ab065842bc
Nazmul Hossain <snazmulhossains24@gmail.com>
2025-07-17 14:44:11 +0600

    User Model Done (Customize)

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from users.managers import CustomUserManager
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = "email"  # ---> Use email instead of username
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
