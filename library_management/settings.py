b9f81dc5bf9b2b5da8a901b3963cdf2c74fdd2a0
Nazmul Hossain <snazmulhossains24@gmail.com>
2025-07-22 23:53:56 +0600

    DB connection --> Supabase

--- library_management/settings.py
+++ library_management/settings.py
@@ -82,10 +82,21 @@ WSGI_APPLICATION = "library_management.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+# -----> Using PostgreSQL as the database backend (Supabase)
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("dbname"),
+        "USER": config("user"),
+        "PASSWORD": config("password"),
+        "HOST": config("host"),
+        "PORT": config("port"),
     }
 }
 
